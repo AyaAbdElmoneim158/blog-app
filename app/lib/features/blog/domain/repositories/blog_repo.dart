3e195eb43c97f779e256f6043a67fe021246306b
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-10-05 09:44:02 +0300

    Add blog.repo Add blog.data.source

---
+++ app/lib/features/blog/domain/repositories/blog_repo.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:fpdart/fpdart.dart';
+import '../../../../core/error/failures.dart';
+import '../entities/blog.dart';
+
+abstract interface class BlogRepo {
+  Future<Either<Failure, Blog>> uploadBlog({
+    required File image,
+    required String title,
+    required String content,
+    required String posterId,
+    required List<String> topics,
+  });
+  Future<Either<Failure, List<Blog>>> getAllBlogs();
+}
